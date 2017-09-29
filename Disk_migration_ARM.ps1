ef17c24d8de15c2337927c93507afedafa8ed3d3
martindude <32392325+martindude@users.noreply.github.com>
2017-09-29 15:50:57 +0200

    Update Disk_migration_ARM.ps1

--- Disk_migration_ARM.ps1
+++ Disk_migration_ARM.ps1
@@ -1,9 +1,9 @@
 ﻿#Provide the subscription Id of the subscription where managed disk exists
-$sourceSubscriptionId='e7cb5307-03f2-4f3e-98bf-799d484d8a30'
+$sourceSubscriptionId='source_ID'
 
 #Provide the name of your resource group where managed disk exists
 $sourceResourceGroupName='RG-EU'
-$managedDiskName='srv-win-01_OsDisk_1_c61ada1bd1964658a291acf5aa5d0905'
+$managedDiskName='os_disk_name'
 
 #Set the context to the subscription Id where Managed Disk exists
 Select-AzureRmSubscription -SubscriptionId $sourceSubscriptionId
@@ -13,10 +13,10 @@ $managedDisk= Get-AzureRMDisk -ResourceGroupName $sourceResourceGroupName -DiskN
 
 #Provide the subscription Id of the subscription where managed disk will be copied to
 #If managed disk is copied to the same subscription then you can skip this step
-$targetSubscriptionId='e7cb5307-03f2-4f3e-98bf-799d484d8a30'
+$targetSubscriptionId='target_ID'
 
 #Name of the resource group where snapshot will be copied to
-$targetResourceGroupName='RG'
+$targetResourceGroupName='target_RG'
 
 #Set the context to the subscription Id where managed disk will be copied to
 #If snapshot is copied to the same subscription then you can skip this step
